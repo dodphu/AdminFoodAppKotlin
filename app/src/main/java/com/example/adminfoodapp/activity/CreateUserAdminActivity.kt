8cc8b02e8b969fbee0bcc88adf105a4b634ffbc2
dodphu <dodphu201@gmail.com>
2024-05-24 01:18:16 +0700

    update

--- app/src/main/java/com/example/adminfoodapp/activity/CreateUserAdminActivity.kt
+++ app/src/main/java/com/example/adminfoodapp/activity/CreateUserAdminActivity.kt
@@ -1,6 +1,7 @@
 package com.example.adminfoodapp.activity
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,6 +20,11 @@ class CreateUserAdminActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.btnCreateAccAdmin.setOnClickListener {
+            Toast.makeText(this, "Đăng nhập vào ứng dụng để đăng kí tài khoản", Toast.LENGTH_SHORT)
+                .show()
         }
     }
 }
